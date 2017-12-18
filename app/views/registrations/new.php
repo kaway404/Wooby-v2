be9a46d0a91ff97fbe93b3962109ccc73ad69637
kaway404 <32229419+kaway404@users.noreply.github.com>
2017-12-18 16:15:34 -0200

    Registro

--- app/views/registrations/new.php
+++ app/views/registrations/new.php
@@ -1,6 +1,54 @@
-<h1>Fazer aqui a vista onde o usuario vai inserir os dados pra se cadastar</h1>
-<p>O formulario deve tar direcionado pra '/registrations/create'</p>
-<p>Os nomes do formularios deven ficar dentro dum array chamado user, exemplo:</p>
-<code>
-	&lt input type="text" name="[User][nome]" &gt
-</code>
+<nav class="navbar navbar-default defaultw">
+  <div class="container-fluid">
+    <!-- Brand and toggle get grouped for better mobile display -->
+    <div class="navbar-header">
+      <a class="navbar-brand" href="/"><img src="/static/logo.png" class="logo"/></a>
+    </div>
+
+    <!-- Collect the nav links, forms, and other content for toggling -->
+    <div class="collapse navbar-collapse" id="bs-example-navbar-collapse-1">
+
+      <ul class="nav navbar-nav navbar-right navw">
+        <li class="login"><a href="login.php">Login</a></li></ul>
+    </div><!-- /.navbar-collapse -->
+  </div><!-- /.container-fluid -->
+</nav>
+
+
+<div class="align-login">
+<div class="loginfs">
+<form class="ui form">
+  <div class="field">
+    <label>Email</label>
+    <input type="email" name="[User][email]" placeholder="E-mail">
+  </div>
+  <div class="field">
+    <label>Senha</label>
+    <input type="password" name="[User][senha]" placeholder="Senha">
+  </div>
+  <div class="field">
+    <label>Nome de usuario</label>
+    <input type="text" name="[User][usuario]" placeholder="Nome de usuario">
+  </div>
+  <div class="field">
+    <label>Nome de quem vai usar a conta</label>
+    <input type="text" name="[User][perfil]" placeholder="Perfil">
+  </div>
+  <div class="field">
+    <div class="ui checkbox">
+      <input type="checkbox" tabindex="0" class="hidden">
+      <label>Concordo com os termos de usos</label>
+    </div>
+  </div>
+  <button class="ui button" type="submit">Registrar</button>
+</form>
+</div>
+</div>
+
+<style>
+.background{
+	background-image: url(http://woobyanimes.com/static/backgronds/1.png);
+}
+</style>
+
+<div class="background"></div>
