c553fe680a5e86c8fcbe289f4187b84741934dec
kaway404 <32229419+kaway404@users.noreply.github.com>
2017-12-19 20:40:18 -0200

    Fix modal login

--- app/views/registrations/new.php
+++ app/views/registrations/new.php
@@ -33,4 +33,31 @@
 }
 </style>
 
+<div class="modal" tabindex="-1" role="dialog" id='login_modal'>
+	<div class="modal-dialog" role="document">
+		<div class="modal-content">
+			<div class="modal-header">
+				<h4 class="modal-title">Login
+				<button type="button" class="close" data-dismiss="modal" aria-label="Close"><span aria-hidden="true">&times;</span></button>
+				</h4>
+			</div>
+			<div class="modal-body">
+				<div class="form-group">
+					<label for="email">Email:</label>
+					<input type="email" class="form-control" id="email">
+				</div>
+				<div class="form-group">
+					<label for="senha">Senha:</label>
+					<input type="password" class="form-control" id="senha">
+				</div>
+			</div>
+			<div class="modal-footer">
+				<button type="button" class="btn btn-primary">Login</button>
+				<button type="button" class="btn btn-secondary" data-dismiss="modal">Fechar</button>
+			</div>
+		</div>
+	</div>
+</div>
+
+
 <div class="background"></div>
